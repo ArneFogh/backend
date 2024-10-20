23ed98bf2390ca74428d9e2e144549a1ef6dc863
arnefogh <andreasfogh7@gmail.com>
2024-10-20 02:20:41 +0200

    updated cors

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,20 +1,25 @@
-const cors = require('cors');
+const cors = require("cors");
 
 const allowedOrigins = [
   "https://welovebirds.dk",
   "https://api.welovebirds.dk",
   "http://localhost:3000",
   "https://onpay.io",
+  "https://welovebirdssanity.sanity.studio", // Tilføjet for Sanity
 ];
 
 module.exports = cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      var msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
-  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 204,
 });
